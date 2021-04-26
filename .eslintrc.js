356c38903c6ee07e351244934f7f8da41acfc898
Sealman <castle2668@gmail.com>
2021-04-26 17:00:53 +0800

    feat: 設定 ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,17 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
-  extends: [
-    'plugin:vue/recommended',
-    '@vue/airbnb',
-  ],
+  extends: ["plugin:vue/recommended", "@vue/airbnb"],
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: "babel-eslint"
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-  },
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    "no-unused-vars": ["off"],
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+  }
 };
